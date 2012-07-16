16cc9d9382466478e45d253b056b7dbf93e6f0c7
crazid0 <id0@free.fr>
2012-07-16 18:22:53 +0200

    main menu 1st try

--- client/android/gen/com/crazyapps/friendlocator/R.java
+++ client/android/gen/com/crazyapps/friendlocator/R.java
@@ -11,13 +11,31 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int button=0x7f020000;
+        public static final int button2=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+    }
+    public static final class id {
+        public static final int ImageButton03=0x7f050003;
+        public static final int callButton=0x7f050004;
+        public static final int contactsButton=0x7f050000;
+        public static final int mapButton=0x7f050001;
+        public static final int settingsButton=0x7f050002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        /**  Main label 
+         */
+        public static final int app_name=0x7f040000;
+        public static final int app_url=0x7f040001;
+        /**  Common label 
+ Main Menu label 
+         */
+        public static final int contacts=0x7f040002;
+        public static final int map=0x7f040003;
+        public static final int settings=0x7f040004;
+        public static final int toCall=0x7f040005;
     }
 }
